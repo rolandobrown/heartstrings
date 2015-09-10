40def9a884d01082e381b9e4554ba5e87e520fcb
staceyko <ko.stacey@gmail.com>
2015-09-09 20:47:20 -0400

    update message model

--- HeartString/app/controllers/messages_controller.rb
+++ HeartString/app/controllers/messages_controller.rb
@@ -69,6 +69,6 @@ class MessagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def message_params
-      params.require(:message).permit(:subject, :body, :published_at, :read, :user_id)
+      params.require(:message).permit(:subject, :body, :published_at, :status, :read, :user_id)
     end
 end
--- HeartString/app/models/message.rb
+++ HeartString/app/models/message.rb
@@ -1,5 +1,23 @@
 class Message < ActiveRecord::Base
   belongs_to :sender, :class_name => "User"
-  belongs_to :reciever, :class_name => "User"
+  belongs_to :receiver, :class_name => "User"
   has_many :keepsakes
+
+  scope :draft,     ->{ where(status: "Draft") }
+  scope :send_now, ->{ where(status: "Send Now") }
+  scope :schedule, ->{ where(status: "Schedule") }
+
+  before_validation :clean_up_status
+
+  def clean_up_status
+    self.published_at = case status
+                        when "Draft"
+                          nil
+                        when "Send Now"
+                          Time.zone.now
+                        else
+                          published_at
+                        end
+    true
+  end
 end
