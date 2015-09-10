865f2531426066e5ee35a33cb3585b1aff77c0df
staceyko <ko.stacey@gmail.com>
2015-09-09 20:52:35 -0400

    add messages helper module

---
+++ HeartString/app/helpers/messages_helper.rb
@@ -0,0 +1,13 @@
+module MessagesHelper
+  def status_for(message)
+    if message.published_at?
+      if message.published_at > Time.zone.now
+        "Schedule"
+      else
+        "Send Now"
+      end
+    else
+      "Draft"
+    end
+  end
+end
