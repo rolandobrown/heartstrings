03cc9b955f948a294190fc7b20bf5410c2088d4f
rolandobrown <rolando.brown@gmail.com>
2015-09-14 23:44:15 -0400

    Fix Asset Path

---
+++ app/inputs/lovedone_input.rb
@@ -0,0 +1,8 @@
+# app/inputs/lovedone_input.rb
+class LovedOneInput < SimpleForm::Inputs::Base
+  def input(wrapper_options)
+    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
+
+    "$ #{@builder.text_field(attribute_name, merged_input_options)}".html_safe
+  end
+end
